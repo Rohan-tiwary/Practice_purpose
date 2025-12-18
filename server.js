8cb28c6dea54f4bdc119da6ce24f3b2a90a8b7e5
Rohan Kumar Tiwary <132188663+Rohan-tiwary@users.noreply.github.com>
2025-12-18 21:51:17 +0530

    added new api

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
     res.send('Hello World!');
 });
 
+app.get('/about',(req,res)=>{
+    res.send('About Page');
+});
+
 app.listen(port,()=>{
     console.log(`Server is running at http://localhost:${port}`);
 });
